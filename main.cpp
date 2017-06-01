1e6af0d868545721add77d7a40e3c135845d4ef9
Huang <hl48@iupui.edu>
2017-06-01 00:15:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main()
     /// random sampling (0.1%)
     vector<float> cTrk;
     size_t iTotalTrackNum = cReader.getTotalTrkNum();  /// total number of tracks in input file
-    srand(unsigned int(time(nullptr)));                           /// random seed
+    srand((unsigned int)(time(nullptr)));                           /// random seed
     for(size_t i = 0; i < iTotalTrackNum; i++)
     {
         if( rand() % 1000 < 998 )   /// 0.1% random sampling
